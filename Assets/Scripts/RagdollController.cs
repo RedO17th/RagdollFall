288a9ae3cdfd71bf4d7584503319ce84a6c3f26a
Sarat <saratovcev-1995@mail.ru>
2023-06-26 20:49:58 +0300

    RagdollController.Part I

---
+++ Assets/Scripts/RagdollController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : BasePlayerController
+{
+    public override void Initialize(IPlayer player)
+    {
+        
+    }
+
+    public override void Enable()
+    {
+        base.Enable();
+    }
+
+    public override void Disable()
+    {
+        base.Disable();
+    }
+
+    public override void Clear()
+    {
+        
+    }
+
+
+}
